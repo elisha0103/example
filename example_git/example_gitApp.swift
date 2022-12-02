c62a4d77a1b8f4739688fbb7119156c22e6a9a19
TaeYoung Jin <elisha0103@naver.com>
2022-12-02 10:33:13 +0900

    second commit

---
+++ example_git/example_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_gitApp.swift
+//  example_git
+//
+//  Created by 진태영 on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct example_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
